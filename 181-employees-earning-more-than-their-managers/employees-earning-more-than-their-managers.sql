389a8d288398464e5ec705f22e11616efa2ef216
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-08-14 16:58:44 -0400

    Time: 390 ms (44.56%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,7 +1,5 @@
--- Write your PostgreSQL query statement below
-SELECT e.name AS Employee
-FROM Employee e
-JOIN Employee e2
-    ON e.managerId = e2.id
-WHERE 
-    e.salary > e2.salary
+# Write your MySQL query statement below
+select e1.name AS Employee
+from employee e1
+inner join employee e2 on e1.managerId = e2.id
+where e1.salary > e2.salary
