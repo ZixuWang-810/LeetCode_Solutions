8cda0e8f868e45efdf53729ecde07042e3d9770a
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-08-18 17:22:01 -0400

    Time: 79 ms (92.91%) | Memory: 23.5 MB (79.96%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,21 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        left = 0
-        fruit_count = {}  # Track fruit types in current window
-        max_fruits = 0
-        
-        for right in range(len(fruits)):
-            # Add current fruit to window
-            fruit_count[fruits[right]] = fruit_count.get(fruits[right], 0) + 1
-            
-            # If we have more than 2 fruit types, shrink window
-            while len(fruit_count) > 2:
-                fruit_count[fruits[left]] -= 1
-                if fruit_count[fruits[left]] == 0:
-                    del fruit_count[fruits[left]]
-                left += 1
-            
-            # Update maximum
-            max_fruits = max(max_fruits, right - left + 1)
-        
-        return max_fruits
+        last = sec_last = -1
+        last_count = curr = res = 0
+        for f in fruits:
+            if f == last or f == sec_last:
+                curr += 1
+            else: curr = last_count + 1
+
+            if f == last:
+                last_count +=1
+            else:
+                last_count = 1
+                sec_last, last = last, f
+            res = max(res, curr)
+        return res
