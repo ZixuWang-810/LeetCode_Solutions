468a594d4337a0eee03da908dc5f0ad0fbc70a37
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-08-18 20:35:07 -0400

    Time: 70 ms (94.80%) | Memory: 23.8 MB (14.33%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,16 +1,17 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         last = sec_last = -1
-        last_count = curr = res = 0
-        for f in fruits:
-            if f == last or f == sec_last:
-                curr += 1
-            else: curr = last_count + 1
+        curr, last_count, res = 0, 0, 0
+        for i in fruits:
+            if i == last or i == sec_last:
+                curr+=1
+            else:
+                curr = last_count + 1
 
-            if f == last:
+            if i == last:
                 last_count +=1
             else:
                 last_count = 1
-                sec_last, last = last, f
+                last, sec_last = i, last
             res = max(res, curr)
         return res
