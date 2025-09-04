dceaa88266cb0b80ae4a70976c6d973e564d74cb
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-09-04 14:21:40 -0400

    Time: 938 ms (67.36%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    p.product_id, 
+    p.product_name
+FROM Product p
+LEFT JOIN Sales s 
+ON s.product_id = p.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MAX(s.sale_date) <= '2019-03-31'
+AND MIN(s.sale_date) >= '2019-01-01'
