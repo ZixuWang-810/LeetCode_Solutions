e0b9b7347f12a85c0466804ba84715a678624439
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-09-05 15:37:55 -0400

    Time: 949 ms (63.04%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,10 +1,8 @@
 -- Write your PostgreSQL query statement below
-SELECT
-    p.product_id, 
-    p.product_name
+SELECT p.product_id, p.product_name
 FROM Product p
-LEFT JOIN Sales s 
-ON s.product_id = p.product_id
+JOIN Sales s
+ON p.product_id = s.product_id
 GROUP BY p.product_id, p.product_name
-HAVING MAX(s.sale_date) <= '2019-03-31'
-AND MIN(s.sale_date) >= '2019-01-01'
+HAVING MIN(s.sale_date) >= '2019-01-01'
+AND MAX(s.sale_date) <= '2019-03-31'
