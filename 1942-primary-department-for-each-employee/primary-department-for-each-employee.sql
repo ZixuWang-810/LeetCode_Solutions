13bb54c67edf16cc99c38e5d67b6ae375e5239af
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-09-05 17:22:47 -0400

    Time: 222 ms (88.98%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,14 +1,10 @@
 -- Write your PostgreSQL query statement below
-
-(select employee_id, department_id
-from employee
-where primary_flag = 'Y')
-
-union
-
-(select employee_id, department_id
-from employee
-where employee_id in (select employee_id
-from employee
-group by employee_id
-having count(distinct department_id) = 1))
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+OR employee_id IN (
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(department_id) = 1
+)
