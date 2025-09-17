da3ea822041b7cc9230a7ec51ba21cbfe70eb52b
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-09-17 19:19:26 -0400

    Time: 378 ms (11.27%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,6 +1,10 @@
--- Write your PostgreSQL query statement below
-SELECT DISTINCT l1.num AS ConsecutiveNums
-FROM Logs l1
-JOIN Logs l2 ON l1.id = l2.id - 1
-JOIN Logs l3 ON l2.id = l3.id - 1
-WHERE l1.num = l2.num AND l2.num = l3.num;
+WITH    cte AS (
+        SELECT            num,
+            lag (num, 1) OVER () AS lag_1,
+            lag (num, 2) OVER () AS lag_2
+        FROM            Logs
+    )
+SELECT DISTINCT    (num) AS ConsecutiveNums
+FROM    cte
+WHERE    num = lag_1
+    AND num = lag_2;
