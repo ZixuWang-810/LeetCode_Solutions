21301b4fd54c907b9270cd7bc403d0e503021002
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-09-17 13:57:53 -0400

    Time: 227 ms (39.50%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,12 +1,12 @@
 -- Write your PostgreSQL query statement below
--- Write your PostgreSQL query statement below
-SELECT id, 
-  CASE WHEN p_id IS NULL THEN 'Root' 
-  WHEN id NOT IN (
-    SELECT p_id 
-    FROM Tree 
-    WHERE p_id IS NOT NULL
+SELECT
+    id,
+    CASE WHEN p_id IS NULL THEN 'Root'
+    WHEN id NOT IN (
+        SELECT p_id
+        FROM Tree
+        WHERE p_id IS NOT NULL
     ) THEN 'Leaf'
-  ELSE 'Inner' 
-  END AS type 
+    ELSE 'Inner' END
+    AS type
 FROM Tree
