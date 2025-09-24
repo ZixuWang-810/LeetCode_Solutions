eea581e366b55b2f658f6251dac72124ffb056c3
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-09-24 13:54:37 -0400

    Time: 374 ms (56.79%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,17 +1,20 @@
 -- Write your PostgreSQL query statement below
-with cte as ( 
-select product_id, new_price from Products 
-where  (product_id, change_date) in (  
-    select product_id, max(change_date) as latest_date
-    from Products
-    where change_date <= '2019-08-16'
-    group by 1 )
+WITH cte AS (
+    SELECT 
+        product_id, 
+        new_price
+    FROM Products
+    WHERE (product_id, change_date) IN (
+        SELECT product_id, MAX(change_date)
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    )
 )
 
-select distinct 
+SELECT DISTINCT 
     p.product_id, 
-    (CASE WHEN cte.new_price IS NULL THEN 10
-    ELSE cte.new_price END) AS price
-from Products p
-left join cte
-on p.product_id = cte.product_id
+    coalesce(cte.new_price,10) AS price 
+FROM Products p
+LEFT JOIN cte
+ON p.product_id = cte.product_id
