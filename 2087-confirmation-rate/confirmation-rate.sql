9a9dd3e8187b16dd090db490caa03d105cd0c7d0
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-01 14:34:15 -0400

    Time: 281 ms (26.43%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -2,10 +2,14 @@
 SELECT 
     s.user_id,
     ROUND(
-        SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) ::DECIMAL / COUNT(s.user_id) ::DECIMAL
+        SUM(
+            CASE 
+                WHEN c.action = 'confirmed' 
+                THEN 1 ELSE 0 
+            END)::DECIMAL 
+        /COUNT(s.user_id)::DECIMAL
         ,2
     )AS confirmation_rate
-
 FROM Signups s
 LEFT JOIN Confirmations c
     ON s.user_id = c.user_id
