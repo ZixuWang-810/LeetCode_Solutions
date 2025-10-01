f8001be6ced1040f19f8a3784cbdec3f4f132d79
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-01 12:42:10 -0400

    Time: 206 ms (68.52%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,18 +1,18 @@
 -- Write your PostgreSQL query statement below
-WITH cte AS (SELECT 
+SELECT 
     p.product_id,
-    ROUND(
-        SUM(p.price * u.units)::DECIMAL / SUM(u.units)
-        ,2
-    ) AS average_price
+    CASE
+        WHEN SUM(u.units) IS NULL 
+        THEN 0
+        ELSE 
+            ROUND(
+                SUM(p.price * u.units)::DECIMAL / SUM(u.units)
+                ,2
+            ) 
+    END
+    AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold u
     ON p.product_id = u.product_id
     AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
-)
-
-SELECT 
-    product_id,
-    coalesce(average_price, 0) AS average_price
-FROM cte
