234f34a9ef609af2a634b37aadad774848afe2c3
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-02 14:28:13 -0400

    Time: 936 ms (5.06%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,10 +1,28 @@
 -- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT
+        TO_CHAR(trans_date,'YYYY-MM') AS month,
+        id,
+        country,
+        state,
+        amount
+    FROM Transactions
+)
 SELECT
-    TO_CHAR(trans_date, 'YYYY-MM') AS month,
+    month,
     country,
     COUNT(id) AS trans_count,
-    COUNT(CASE WHEN state = 'approved' THEN 1 END) AS approved_count,
+    COUNT((CASE
+            WHEN state = 'approved' THEN 1 
+        END)
+    ) AS approved_count,
     SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
-FROM Transactions
-GROUP BY month, country
+    SUM(CASE
+            WHEN state = 'approved' THEN amount
+            ELSE 0
+        END
+    ) AS approved_total_amount
+FROM cte
+GROUP BY   
+    month,
+    country
