ca83240214893de7eff78dcd33c9009b4733082e
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-02 11:32:35 -0400

    Time: 497 ms (75.72%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,30 +1,25 @@
--- Write your PostgreSQL query statement below
 WITH cte AS (
     SELECT
         player_id,
         event_date,
-        LEAD(event_date) OVER(
-            PARTITION BY player_id ORDER BY event_date
-        ) AS next_date
+        lead(event_date) OVER(
+            PARTITION BY player_id
+            ORDER BY event_date
+        ) AS next_date,
+        RANK() OVER(
+            PARTITION BY player_id
+            ORDER BY event_date
+        ) AS date_rank
     FROM Activity
 )
-, cte2 AS (
-    SELECT 
-        DISTINCT player_id
-    FROM cte
-    WHERE next_date = event_date + 1 
-    AND (player_id,event_date) IN (
-        SELECT 
-            player_id,
-            MIN(event_date)
-        FROM cte
-        GROUP BY 1
-    )
-)
-SELECT 
+
+SELECT
     ROUND(
-        COUNT(player_id) ::DECIMAL
-        / (SELECT COUNT(DISTINCT player_id) FROM Activity) ::DECIMAL
+        COUNT(player_id) ::DECIMAL/
+        (SELECT COUNT(DISTINCT player_id) FROM Activity) ::DECIMAL
         ,2
     ) AS fraction
-FROM cte2
+FROM cte
+WHERE 
+    next_date = event_date + 1
+    AND date_rank = 1
