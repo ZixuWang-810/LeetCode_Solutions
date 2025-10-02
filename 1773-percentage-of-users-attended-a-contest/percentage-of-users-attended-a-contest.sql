ddb4dc6363b1d9c91c497df8e2a05086ebcc437c
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-02 14:16:55 -0400

    Time: 1085 ms (29.05%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,19 +1,13 @@
 -- Write your PostgreSQL query statement below
-WITH cte AS (
-    SELECT 
-        COUNT(user_id) AS num_u
-    FROM Users
-)
 SELECT
-    r.contest_id,
+    contest_id,
     ROUND(
-        COUNT(r.user_id) * 100::DECIMAL / ( SELECT num_u FROM cte)
+        COUNT(DISTINCT(user_id))*100::DECIMAL / 
+        (SELECT COUNT(DISTINCT user_id) FROM Users)::DECIMAL
         ,2
     ) AS percentage
-FROM Register r
-LEFT JOIN Users u
-    ON u.user_id = r.user_id
-GROUP BY r.contest_id
-ORDER BY
+FROM Register
+GROUP BY contest_id
+ORDER BY 
     percentage DESC,
-    r.contest_id
+    contest_id
