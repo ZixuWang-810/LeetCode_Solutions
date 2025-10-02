dbcad910f3bfe1be1c831ee7ae0b3fab9a7a7014
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-02 11:37:58 -0400

    Time: 709 ms (9.91%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,25 +1,14 @@
-WITH cte AS (
-    SELECT
-        player_id,
-        event_date,
-        lead(event_date) OVER(
-            PARTITION BY player_id
-            ORDER BY event_date
-        ) AS next_date,
-        RANK() OVER(
-            PARTITION BY player_id
-            ORDER BY event_date
-        ) AS date_rank
-    FROM Activity
-)
-
 SELECT
     ROUND(
-        COUNT(player_id) ::DECIMAL/
-        (SELECT COUNT(DISTINCT player_id) FROM Activity) ::DECIMAL
+        COUNT(DISTINCT player_id)::DECIMAL
+        /(SELECT COUNT(DISTINCT player_id) FROM Activity)::DECIMAL
         ,2
     ) AS fraction
-FROM cte
-WHERE 
-    next_date = event_date + 1
-    AND date_rank = 1
+FROM Activity
+WHERE (player_id, event_date) IN (
+    SELECT
+        player_id,
+        MIN(event_date) + 1
+    FROM Activity
+    GROUP BY 1
+)
