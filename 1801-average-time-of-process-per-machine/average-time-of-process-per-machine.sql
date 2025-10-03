bf4a1846b15ca6cd17d3e13441c8ab4d57aecf0a
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-02 21:15:41 -0400

    Time: 179 ms (46.41%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,23 +1,11 @@
 -- Write your PostgreSQL query statement below
-WITH cte AS (
-    SELECT
-        machine_id,
-        process_id,
-        lead(timestamp) OVER(
-            PARTITION BY 
-                machine_id,
-                process_id
-            ORDER BY timestamp
-        ) AS end_time
-    FROM Activity     
-)
-
 SELECT
-    a.machine_id,
-    ROUND(AVG(c.end_time - a.timestamp) ::DECIMAL , 3) AS processing_time
-FROM Activity a
-LEFT JOIN cte c
-    ON a.machine_id = c.machine_id
-    AND a.process_id = c.process_id
-WHERE a.activity_type = 'start'
-GROUP BY a.machine_id
+    machine_id,
+    ROUND(
+        AVG((CASE
+        WHEN activity_type = 'start' THEN -timestamp
+        ELSE timestamp
+    END
+    ))::NUMERIC*2, 3) AS processing_time
+FROM Activity
+GROUP BY machine_id
