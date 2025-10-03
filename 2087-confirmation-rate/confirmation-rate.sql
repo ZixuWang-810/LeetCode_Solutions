5e0951af5e507caa7b16861c6329714104809139
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-02 21:38:45 -0400

    Time: 259 ms (44.80%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,16 +1,18 @@
 -- Write your PostgreSQL query statement below
-SELECT 
-    s.user_id,
-    coalesce(ROUND(
-        AVG(
-            CASE
-                WHEN c.action = 'confirmed' THEN 1
-                ELSE 0
-            END
-        )::DECIMAL
-        ,2
-    ), 0) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c
-ON s.user_id = c.user_id
-GROUP BY s.user_id
+WITH cte AS (
+    SELECT 
+        s1.user_id,
+        c.time_stamp,
+        c.action
+    FROM Signups s1
+    LEFT JOIN Confirmations c
+    ON s1.user_id = c.user_id
+)
+
+SELECT
+    user_id,
+    ROUND(AVG(CASE WHEN action = 'confirmed' THEN 1.00 
+    ELSE 0 END), 2) AS confirmation_rate
+FROM cte
+GROUP BY 
+    user_id
