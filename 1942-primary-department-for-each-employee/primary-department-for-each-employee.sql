f62b5261f637f6a374c5a430ec9dff79f5c34e5f
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-03 22:45:45 -0400

    Time: 214 ms (98.86%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,13 +1,16 @@
 -- Write your PostgreSQL query statement below
-SELECT
+WITH cte AS (SELECT
+    employee_id
+FROM Employee
+GROUP BY 1
+HAVING COUNT(*) = 1)
+-- UNION
+(SELECT 
     employee_id,
     department_id
 FROM Employee
 WHERE primary_flag = 'Y'
-OR
-primary_flag = 'N' AND employee_id IN (
-    SELECT employee_id
-    FROM Employee
-    GROUP BY employee_id
-    HAVING COUNT(employee_id) = 1
+OR employee_id IN (
+    SELECT * FROM cte 
+)
 )
