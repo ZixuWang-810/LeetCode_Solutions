ffabd2c31b7274c180d14f4cf72a11f9e7f847db
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-07 13:50:49 -0400

    Time: 204 ms (35.23%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,14 +1,23 @@
 -- Write your PostgreSQL query statement below
-WITH day_sum_table AS (
-    SELECT  visited_on, SUM(amount) AS day_sum
+WITH cte AS (
+    SELECT
+        visited_on,
+        SUM(amount) AS amount
     FROM Customer
     GROUP BY visited_on
     ORDER BY visited_on
 )
 
-SELECT visited_on,
-SUM(day_sum) OVER (ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) as amount,
-ROUND(AVG(day_sum) OVER (ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) as average_amount
-
-FROM day_sum_table
+SELECT
+    visited_on,
+    SUM(amount) OVER(
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(
+        AVG(amount::DECIMAL) OVER(
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)
+        ,2
+    ) AS average_amount 
+FROM cte
+ORDER BY 1
 OFFSET 6
