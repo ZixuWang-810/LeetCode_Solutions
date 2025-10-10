ba689912503a2ae6259ab4a3423c237fd2cf476e
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-09 22:05:13 -0400

    Time: 884 ms (96.97%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -2,12 +2,13 @@
 SELECT
     contest_id,
     ROUND(
-        COUNT(DISTINCT(user_id))*100::DECIMAL / 
-        (SELECT COUNT(DISTINCT user_id) FROM Users)::DECIMAL
+        COUNT(user_id)*100::DECIMAL / (
+            SELECT COUNT(*) FROM Users
+        )::DECIMAL
         ,2
     ) AS percentage
 FROM Register
 GROUP BY contest_id
 ORDER BY 
-    percentage DESC,
+    percentage DESC, 
     contest_id
