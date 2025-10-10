4b4912109714031328d1e6727d281e513e21a5c1
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-09 23:35:05 -0400

    Time: 486 ms (82.98%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,10 +1,12 @@
 -- Write your PostgreSQL query statement below
 SELECT
-    c.customer_id
-FROM Customer c
-GROUP BY c.customer_id
-HAVING COUNT(DISTINCT c.product_key) = (
-    SELECT
-        COUNT(DISTINCT product_key)
-    FROM Product
+    customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING (
+    COUNT(DISTINCT product_key) = (
+        SELECT
+            COUNT(product_key)
+        FROM Product
+    )
 )
