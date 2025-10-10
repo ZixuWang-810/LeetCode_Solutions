a055ade38634c3508a6d381a789e3e7e901bd044
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-09 22:46:44 -0400

    Time: 507 ms (6.08%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -5,8 +5,7 @@ SELECT
     COUNT(*) AS reports_count,
     AVG(e1.age)::INTEGER AS average_age
 FROM Employees e1
-LEFT JOIN Employees e2
+JOIN Employees e2
 ON e1.reports_to = e2.employee_id
-WHERE e2.employee_id IS NOT NULL
 GROUP BY e2.employee_id, e2.name
 ORDER BY e2.employee_id
