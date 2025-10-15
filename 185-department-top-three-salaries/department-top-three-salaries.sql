061a6e22ed9ca64fdd2c46c7cc73317c4c595809
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-14 23:40:10 -0400

    Time: 244 ms (83.29%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -7,11 +7,11 @@ WITH cte AS (
         DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rank
     FROM Employee
 )
-SELECT
-    d.name AS Department ,
-    c.name AS Employee ,
+SELECT 
+    d.name AS Department,
+    c.name AS Employee,
     c.salary AS Salary
-FROM cte c 
+FROM cte c
 LEFT JOIN Department d
-ON c.departmentId = d.id
-WHERE rank < 4
+ON d.id = c.departmentId
+WHERE c.rank < 4
