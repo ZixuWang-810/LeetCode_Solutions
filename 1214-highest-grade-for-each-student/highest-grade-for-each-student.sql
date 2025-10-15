e1da099fd9626bda0cadc5c9cfcc5efeff8b07d5
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-15 19:01:05 -0400

    Time: 490 ms (76.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT
+        student_id,
+        course_id,
+        grade,
+        RANK() OVER(PARTITION BY student_id ORDER BY grade DESC, course_id ASC) AS rank
+    FROM Enrollments
+)
+SELECT
+    student_id,
+    course_id,
+    grade
+FROM cte
+WHERE rank = 1
+ORDER BY student_id
