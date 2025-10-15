209b4b0dcd8eb37573e58bd3a369cb97c2beee26
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-15 00:06:22 -0400

    Time: 179 ms (78.04%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,9 +1,10 @@
 -- Write your PostgreSQL query statement below
 SELECT
-    (CASE WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
-    WHEN id % 2 = 1 THEN id +1
-    ELSE id - 1
-    END) AS id,
+    CASE
+        WHEN id = (SELECT COUNT(*) FROM Seat) AND id % 2 = 1 THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1 
+    END AS id,
     student
 FROM Seat
 ORDER BY id
