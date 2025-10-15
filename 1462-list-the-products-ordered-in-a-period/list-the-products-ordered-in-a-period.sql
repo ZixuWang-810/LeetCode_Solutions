9a2caa0f1917c274a48faa7817496d109e3f6bd7
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-15 11:03:56 -0400

    Time: 235 ms (32.99%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,18 +1,7 @@
--- Write your PostgreSQL query statement below
-WITH cte AS (
-    SELECT
-        product_id,
-        SUM(unit) AS amount
-    FROM orders
-    WHERE order_date >= '2020-02-01'
-    AND order_date < '2020-03-01'
-    GROUP BY product_id
-)
-
-SELECT
-    p.product_name,
-    c.amount AS unit
-FROM cte c
-LEFT JOIN Products p
-ON c.product_id = p.product_id
-WHERE amount >= 100
+SELECT product_name, SUM(unit) AS unit
+FROM Orders AS o
+INNER JOIN Products
+USING(product_id)
+WHERE TO_CHAR(order_date, 'YYYY-MM') = '2020-02'
+GROUP BY product_id, product_name
+HAVING SUM(unit) >= 100;
