bdfac038f22109c06ca41904d617b4a4227d3b78
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-15 23:34:40 -0400

    Time: 234 ms (94.47%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,22 +1,38 @@
 -- Write your PostgreSQL query statement below
-(SELECT
-    u.name AS results
-FROM Users u
-LEFT JOIN MovieRating m
-ON u.user_id = m.user_id
-GROUP BY m.user_id, u.name
-ORDER BY COUNT(*) DESC, u.name
-LIMIT 1) 
+WITH cte1 AS  (
+    SELECT
+        u.name,
+        COUNT(*) AS num
+    FROM Users u
+    LEFT JOIN MovieRating m
+    ON u.user_id = m.user_id
+    GROUP BY u.name
+    ORDER BY u.name
+
+)
+, cte2 AS (
+    SELECT
+        m.title,
+        AVG(mr.rating) as avg
+    FROM MovieRating mr
+    LEFT JOIN Movies m
+    ON m.movie_id = mr.movie_id
+    WHERE mr.created_at >= '2020-02-01'
+    AND mr.created_at < '2020-03-01'
+    GROUP BY m.title
+    ORDER BY m.title
+)
+
+(
+    SELECT name AS results
+    FROM cte1
+    WHERE num = (SELECT MAX(num) FROM cte1)
+    LIMIT 1
+)
 UNION ALL
 (
-    SELECT 
-        m.title
-    FROM Movies m
-    LEFT JOIN MovieRating mr
-    ON mr.movie_id = m.movie_id
-    WHERE created_at >= '2020-02-01' AND 
-    created_at < '2020-03-01'
-    GROUP BY mr.movie_id, m.title
-    ORDER BY AVG(rating::DECIMAL) DESC, m.title
+    SELECT title
+    FROM cte2
+    WHERE avg = (SELECT MAX(avg) FROM cte2)
     LIMIT 1
 )
