83276ab551e237caa9f8248f41f1cd5240187c64
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-16 13:46:48 -0400

    Time: 383 ms (28.96%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,11 +1,13 @@
 -- Write your PostgreSQL query statement below
-SELECT 
+SELECT
     name
-FROM SalesPerson
+FROM SalesPerson 
 WHERE sales_id NOT IN (
     SELECT sales_id
-    FROM Orders o
-    LEFT JOIN Company c
-    ON c.com_id = o.com_id
-    WHERE c.name = 'RED'
+    FROM Orders
+    WHERE com_id = (
+        SELECT com_id
+        FROM Company 
+        WHERE name = 'RED'
+    )
 )
