2433f9e7ca4110d748da19e632e39bb10b946b49
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-15 23:14:31 -0400

    Time: 406 ms (70.49%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,6 +1,8 @@
+-- Write your PostgreSQL query statement below
 SELECT
     user_id,
     (UPPER(SUBSTRING(name FROM 1 FOR 1)) ||
-    LOWER(SUBSTRING(name FROM 2))) AS name
+    LOWER(SUBSTRING(name FROM 2))
+    ) AS name
 FROM Users
 ORDER BY user_id
