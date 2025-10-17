37ae90396e82bd1df0d867b54d37677952404d53
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-16 22:32:00 -0400

    Time: 406 ms (40.07%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -1,4 +1,5 @@
 -- Write your PostgreSQL query statement below
 SELECT *
-FROM users
-WHERE mail ~ '^[a-zA-Z]+[a-zA-Z0-9_.-]*@leetcode\.com$'
+FROM Users
+WHERE 
+    mail ~ '^[a-zA-Z]+[a-zA-Z0-9_.-]*@leetcode\.com$'
