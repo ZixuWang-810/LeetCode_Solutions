668c21619596f5851ff80fde9a43f259440f0145
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-17 19:31:26 -0400

    Time: 192 ms (95.41%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,13 +1,17 @@
 -- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT
+        p.product_id,
+        p.price, 
+        u.units
+    FROM Prices p
+    LEFT JOIN UnitsSold u 
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date 
+    AND p.end_date
+)
 SELECT
-    p.product_id,
-    COALESCE(ROUND(
-        (SUM(u.units*p.price)::DECIMAL )/ (SUM(u.units)::DECIMAL)
-        ,2
-    ), 0)AS average_price
-FROM UnitsSold u
-RIGHT JOIN Prices p
-ON u.product_id = p.product_id
-AND u.purchase_date BETWEEN p.start_date
-AND p.end_date
-GROUP BY p.product_id
+    product_id,
+    COALESCE(ROUND(SUM(units * price) ::DECIMAL / SUM(units::DECIMAL), 2), 0) AS average_price 
+FROM cte
+GROUP BY product_id
