e7ab285bcd2f04c692cd8b11a03f6a0723334a1f
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-16 22:20:49 -0400

    Time: 918 ms (60.12%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,14 +1,17 @@
 -- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT
+        product_id,
+        year,
+        quantity,
+        price,
+        RANK() OVER(PARTITION BY product_id ORDER BY year) AS year_rank
+    FROM Sales
+)
 SELECT
     product_id,
     year AS first_year,
     quantity,
     price
-FROM Sales
-WHERE (product_id, year) IN (
-    SELECT
-        product_id,
-        MIN(year)
-    FROM Sales
-    GROUP BY 1
-)
+FROM cte 
+WHERE year_rank = 1
