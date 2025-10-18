26682e410016a346f476e525bbdcf191b224b9c9
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-17 20:34:47 -0400

    Time: 451 ms (29.41%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -2,17 +2,17 @@
 SELECT
     s.student_id,
     s.student_name,
-    sub.subject_name,
+    su.subject_name,
     COALESCE(COUNT(e.subject_name), 0) AS attended_exams 
 FROM Students s
-CROSS JOIN Subjects sub
+CROSS JOIN Subjects su
 LEFT JOIN Examinations e
-ON s.student_id = e.student_id
-AND sub.subject_name = e.subject_name
+    ON e.student_id = s.student_id
+    AND e.subject_name = su.subject_name
 GROUP BY 
     s.student_id,
     s.student_name,
-    sub.subject_name
+    su.subject_name
 ORDER BY 
     s.student_id,
-    sub.subject_name
+    su.subject_name
