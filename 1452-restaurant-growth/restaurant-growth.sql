39ce7b80ea6daff7fc6ed3f67b9c9bdf3afa4ce8
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-19 19:02:21 -0400

    Time: 208 ms (30.35%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -7,9 +7,14 @@ WITH cte AS (
     GROUP BY visited_on
     ORDER BY visited_on
 )
-SELECT 
+
+SELECT
     visited_on,
-    SUM(amount) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
-    ROUND(AVG(amount::DECIMAL) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount 
+    SUM(amount) OVER(
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(AVG(amount) OVER(
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ), 2) AS average_amount
 FROM cte
 OFFSET 6
