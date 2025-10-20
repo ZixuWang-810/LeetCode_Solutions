974b05e7f2ec727a25e1a8902517412186cac8c0
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-19 21:29:57 -0400

    Time: 305 ms (93.83%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,13 +1,17 @@
 -- Write your PostgreSQL query statement below
 SELECT
-    e2.employee_id,
-    e2.name,
-    COUNT(*) AS reports_count,
-    AVG(e1.age)::INTEGER AS average_age
+    e1.employee_id,
+    e1.name,
+    COUNT(e2.employee_id) AS reports_count,
+    AVG(e2.age)::INTEGER AS average_age
 FROM Employees e1
-JOIN Employees e2
-ON e1.reports_to = e2.employee_id
-GROUP BY 
-    e2.employee_id,
-    e2.name
-ORDER BY e2.employee_id
+LEFT JOIN Employees e2
+    ON e1.employee_id = e2.reports_to
+WHERE e1.employee_id IN (
+    SELECT reports_to
+    FROM Employees
+)
+GROUP BY
+    e1.employee_id,
+    e1.name
+ORDER BY e1.employee_id
