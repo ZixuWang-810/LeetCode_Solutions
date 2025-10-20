e0800edc913ad52654eb9c2ad1f638337e1646ea
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-20 11:48:06 -0400

    Time: 199 ms (87.83%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1718-warehouse-manager/warehouse-manager.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    w.name AS warehouse_name,
+    SUM(
+        p.Width * p.Length * p.Height * w.units
+    ) AS volume
+FROM Warehouse w
+LEFT JOIN Products p
+    ON p.product_id = w.product_id
+GROUP BY 
+    w.name
