51a9f123a4ad5b10c16fc5cc58be1c3c196960cc
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-21 17:34:44 -0400

    Time: 670 ms (85.71%) | Memory: 0B (100.00%) - LeetSync

--- 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -1,17 +1,26 @@
 -- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT
+        e.left_operand,
+        e.operator,
+        e.right_operand,
+        v1.value AS left_v,
+        v2.value AS right_v
+    FROM Expressions e
+    INNER JOIN Variables v1
+        ON v1.name = e.left_operand
+    INNER JOIN Variables v2
+        ON v2.name = e.right_operand
+)
 SELECT
-    e.left_operand,
-    e.operator,
-    e.right_operand,
-    CASE
-        WHEN (operator = '>' AND v1.value > v2.value)
-        OR (operator = '<' AND v1.value < v2.value)
-        OR (operator = '=' AND v1.value = v2.value)
+    left_operand,
+    operator,
+    right_operand,
+    CASE 
+        WHEN operator = '>' AND left_v > right_v
+        OR operator = '<' AND left_v < right_v
+        OR operator = '=' AND left_v = right_v
         THEN 'true'
         ELSE 'false'
     END AS value
-FROM Expressions e
-INNER JOIN Variables v1
-    ON v1.name = e.left_operand
-INNER JOIN Variables v2
-    ON v2.name = e.right_operand
+FROM cte 
