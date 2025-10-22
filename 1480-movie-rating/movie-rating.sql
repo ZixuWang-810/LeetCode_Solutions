08a05feaba25c93fc3b0fe3ce4a38cf91fbbc21d
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-22 16:08:26 -0400

    Time: 245 ms (73.13%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,38 +1,22 @@
 -- Write your PostgreSQL query statement below
-WITH cte1 AS  (
-    SELECT
-        u.name,
-        COUNT(*) AS num
-    FROM Users u
-    LEFT JOIN MovieRating m
+(SELECT
+    u.name AS results
+FROM MovieRating m
+LEFT JOIN Users u
     ON u.user_id = m.user_id
-    GROUP BY u.name
-    ORDER BY u.name
-
-)
-, cte2 AS (
-    SELECT
-        m.title,
-        AVG(mr.rating) as avg
-    FROM MovieRating mr
-    LEFT JOIN Movies m
-    ON m.movie_id = mr.movie_id
-    WHERE mr.created_at >= '2020-02-01'
-    AND mr.created_at < '2020-03-01'
-    GROUP BY m.title
-    ORDER BY m.title
-)
-
-(
-    SELECT name AS results
-    FROM cte1
-    WHERE num = (SELECT MAX(num) FROM cte1)
-    LIMIT 1
-)
+GROUP BY 1
+ORDER BY COUNT(*) DESC, 1
+LIMIT 1)
 UNION ALL
-(
-    SELECT title
-    FROM cte2
-    WHERE avg = (SELECT MAX(avg) FROM cte2)
-    LIMIT 1
-)
+(SELECT
+    m.title
+FROM MovieRating mr
+LEFT JOIN Movies m
+    ON m.movie_id = mr.movie_id
+WHERE mr.created_at >= '2020-02-01'
+AND mr.created_at < '2020-03-01'
+GROUP BY 
+    1,
+    mr.movie_id
+ORDER BY AVG(rating) DESC, 1
+LIMIT 1)
