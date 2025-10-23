f7522b1377b67f6adbbec5eb0144ad2aa29524a2
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-23 14:26:26 -0400

    Time: 383 ms (17.27%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -0,0 +1,6 @@
+SELECT DISTINCT l1.account_id
+FROM loginfo l1 
+JOIN loginfo l2 
+    ON l1.account_id = l2.account_id 
+AND l1.ip_address != l2.ip_address 
+AND l1.login BETWEEN l2.login AND l2.logout
