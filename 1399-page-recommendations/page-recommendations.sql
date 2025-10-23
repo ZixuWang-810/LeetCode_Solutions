47b006246e4ef8eb48fb56a1ac84d8038b0cc06e
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-23 13:28:14 -0400

    Time: 214 ms (28.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1399-page-recommendations/page-recommendations.sql
@@ -0,0 +1,27 @@
+-- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT
+        user1_id,
+        user2_id
+    FROM Friendship
+    WHERE user1_id = 1
+    UNION ALL
+    SELECT
+        user2_id,
+        user1_id
+    FROM Friendship
+    WHERE user2_id = 1
+)
+SELECT
+    DISTINCT page_id AS recommended_page
+FROM Likes 
+WHERE page_id NOT IN(
+    SELECT page_id
+    FROM Likes
+    WHERE user_id = 1
+)
+AND 
+user_id IN (
+    SELECT user2_id
+    FROM cte
+)
