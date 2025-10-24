ee980eaf3f2c341d91eb3a228c7c511fca623f07
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-24 13:30:43 -0400

    Time: 528 ms (49.76%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,15 +1,24 @@
-SELECT 
-    ROUND(
-        1.0 * COUNT(player_id) / 
-        (SELECT COUNT(DISTINCT player_id)
-        FROM Activity)
-        , 2
-    ) AS fraction
-FROM Activity
-WHERE (player_id, event_date) IN (
-    SELECT 
-        player_id, 
-        MIN(event_date) + 1
+-- Write your PostgreSQL query statement below
+WITH cte AS (
+    SELECT
+        player_id,
+        event_date,
+        LEAD(event_date) OVER(
+            PARTITION BY player_id
+            ORDER BY event_date
+        ) AS next_date
     FROM Activity
+)
+SELECT
+    ROUND(
+        COUNT(DISTINCT player_id)::DECIMAL / 
+        (SELECT COUNT(DISTINCT player_id)::DECIMAL  FROM Activity)
+        ,2
+    ) AS fraction
+FROM cte 
+WHERE (player_id, event_date) IN (
+    SELECT player_id, MIN(event_date)
+    FROM cte 
     GROUP BY player_id
 )
+AND next_date = event_date + 1
