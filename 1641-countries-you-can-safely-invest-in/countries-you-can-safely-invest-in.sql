c00041e00c89b34def18a37076f9b00d96a9c822
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-24 15:23:57 -0400

    Time: 436 ms (74.05%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,33 +1,33 @@
 -- Write your PostgreSQL query statement below
 WITH cte1 AS (
     SELECT
-        p.id,
-        c.name,
-        c.country_code
-    FROM Person p
-    LEFT JOIN Country c
-        ON c.country_code = SUBSTRING(p.phone_number FROM 1 FOR 3)
-)
-,cte2 AS (
-    SELECT
-        caller_id AS id,
-        duration
-    FROM Calls
-    UNION ALL
-    SELECT 
+        caller_id,
         callee_id,
         duration
     FROM Calls
-)
-
-
-SELECT
-    c1.name AS country
-FROM cte2 c2
-LEFT JOIN cte1 c1 
-    ON c1.id = c2.id
-GROUP BY 1, c1.country_code
-HAVING AVG(duration)::DECIMAL > (
-    SELECT AVG(duration)::DECIMAL
+    UNION ALL
+    SELECT
+        callee_id,
+        caller_id,
+        duration
     FROM Calls
 )
+,cte2 AS (
+    SELECT
+        p.id,
+        c.country_code,
+        c.name
+    FROM Person p
+    LEFT JOIN Country c
+        ON SUBSTRING(p.phone_number FROM 1 FOR 3) = c.country_code
+)
+SELECT
+    c2.name AS country
+FROM cte1 c1
+LEFT JOIN cte2 c2
+    ON c1.caller_id = c2.id
+GROUP BY 
+    c2.country_code,
+    1
+HAVING 
+    AVG(c1.duration) > (SELECT AVG(duration) FROM Calls)
