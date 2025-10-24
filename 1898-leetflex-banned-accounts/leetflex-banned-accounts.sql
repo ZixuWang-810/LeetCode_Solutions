a9972b59d0f51aa0611f138599d6f4410a4b3305
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-24 14:58:02 -0400

    Time: 323 ms (47.83%) | Memory: 0B (100.00%) - LeetSync

--- 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -1,6 +1,9 @@
-SELECT DISTINCT l1.account_id
-FROM loginfo l1 
-JOIN loginfo l2 
-    ON l1.account_id = l2.account_id 
-AND l1.ip_address != l2.ip_address 
+-- Write your PostgreSQL query statement below
+SELECT
+    DISTINCT l1.account_id
+FROM LogInfo l1
+INNER JOIN LogInfo l2
+    ON l1.account_id = l2.account_id
+WHERE l1.ip_address != l2.ip_address
 AND l1.login BETWEEN l2.login AND l2.logout
+
