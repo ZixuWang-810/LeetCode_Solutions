e977f52f960cb4a7b84ba33ccbdea3ab185f18d9
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-24 14:52:42 -0400

    Time: 218 ms (43.62%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,9 +1,8 @@
 -- Write your PostgreSQL query statement below
-SELECT 
-    COALESCE(t1.employee_id, t2.employee_id) AS employee_id
-FROM Employees t1 
-FULL JOIN Salaries t2
-    USING (employee_id)
-WHERE t1.name IS NULL 
-OR t2.salary IS NULL
-ORDER BY 1 
+SELECT
+    COALESCE(e.employee_id, s.employee_id) AS employee_id
+FROM Employees e
+FULL JOIN Salaries s
+    ON e.employee_id = s.employee_id
+WHERE s.salary IS NULL 
+OR e.name IS NULL
