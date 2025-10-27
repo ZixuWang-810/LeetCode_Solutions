70e5de5c9a813973edc9c3e0e7bc6b5bc6e789b8
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-27 19:41:10 -0400

    Time: 278 ms (16.37%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,9 +1,15 @@
 -- Write your PostgreSQL query statement below
 SELECT
     query_name,
-    ROUND(AVG(rating::DECIMAL/position::DECIMAL), 2) AS quality ,
     ROUND(
-        COUNT(CASE WHEN rating < 3 THEN 1 END)::DECIMAL / COUNT(*)::DECIMAL
-    , 4) * 100 AS poor_query_percentage 
+        AVG(rating::DECIMAL / position::DECIMAL)
+        ,2
+    ) AS quality,
+    ROUND(
+        COUNT(*) FILTER (WHERE rating < 3)*100::DECIMAL / 
+        COUNT(*)
+        ,2
+    ) AS poor_query_percentage
 FROM Queries
-GROUP BY query_name
+GROUP BY 
+    1
