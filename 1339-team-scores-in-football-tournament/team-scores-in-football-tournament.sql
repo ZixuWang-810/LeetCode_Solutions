419035a76c72cd11ce950d4a1dbf93c8dc604438
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-27 19:04:57 -0400

    Time: 214 ms (77.17%) | Memory: 0B (100.00%) - LeetSync

--- 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -1,37 +1,32 @@
 -- Write your PostgreSQL query statement below
 WITH cte AS (
     SELECT
-        m1.host_team AS team_id,
-        m1.host_goals AS h_goals,
-        m1.guest_goals AS g_goals
-    FROM Teams t
-    INNER JOIN Matches m1
-        ON m1.host_team = t.team_id
+        host_team,
+        guest_team,
+        host_goals,
+        guest_goals
+    FROM Matches
     UNION ALL
-    SELECT 
-        m2.guest_team,
-        m2.guest_goals,
-        m2.host_goals
-    FROM Teams t
-    INNER JOIN Matches m2
-        ON m2.guest_team = t.team_id
+    SELECT
+        guest_team,
+        host_team,
+        guest_goals,
+        host_goals  
+    FROM Matches
 )
-SELECT 
+SELECT
     t.team_id,
     t.team_name,
-    SUM(
-        CASE 
-            WHEN h_goals > g_goals THEN 3
-            WHEN h_goals = g_goals THEN 1
-            ELSE 0
-        END
-    ) AS num_points
+    SUM(CASE 
+        WHEN c.host_goals > c.guest_goals THEN  3
+        WHEN c.host_goals = c.guest_goals THEN  1
+        ELSE 0
+    END) AS num_points
 FROM Teams t
 LEFT JOIN cte c
-    ON t.team_id = c.team_id
+    ON c.host_team = t.team_id
 GROUP BY 
-    t.team_id,
-    t.team_name
+    1, 2
 ORDER BY 
     num_points DESC,
-    t.team_id
+    1
