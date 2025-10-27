a50d54a27a03b1edecc8b71671e75a4a563583a0
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-27 18:34:12 -0400

    Time: 489 ms (76.28%) | Memory: 0B (100.00%) - LeetSync

--- 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
+++ 1214-highest-grade-for-each-student/highest-grade-for-each-student.sql
@@ -1,15 +1,29 @@
 -- Write your PostgreSQL query statement below
-SELECT
-    student_id,
-    MIN(course_id) AS course_id,
-    grade
-FROM Enrollments
-WHERE (student_id, grade) IN (
+WITH cte AS (
     SELECT
         student_id,
-        MAX(grade)
+        course_id,
+        grade
     FROM Enrollments
+    WHERE (student_id, grade) IN (
+        SELECT
+            student_id,
+            MAX(grade)
+        FROM Enrollments
+        GROUP BY 1
+    )
+    ORDER BY course_id
+)
+SELECT
+    student_id,
+    course_id,
+    grade
+FROM cte 
+WHERE (student_id, course_id) IN (
+    SELECT
+        student_id,
+        MIN(course_id)
+    FROM cte
     GROUP BY 1
 )
-GROUP BY 1, 3
-ORDER BY 1
+ORDER BY student_id
