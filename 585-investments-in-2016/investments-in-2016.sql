1ae68d7366dd7688066084c8864706647a85d62b
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-28 20:38:26 -0400

    Time: 309 ms (22.82%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,18 +1,16 @@
 -- Write your PostgreSQL query statement below
-WITH cte AS (
-    SELECT 
-        pid,
-        COUNT(tiv_2015) OVER(PARTITION BY tiv_2015) AS c_5,
-        COUNT(*) OVER(PARTITION BY (lat, lon)) AS c_l,
-        tiv_2016
-    FROM Insurance
-)
-
 SELECT
-    ROUND(
-        SUM(tiv_2016::DECIMAL)
-        ,2
-    ) AS tiv_2016
-FROM cte 
-WHERE c_5 > 1
-AND c_l = 1
+    SUM(ROUND(tiv_2016::DECIMAL, 2)) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY 1
+    HAVING COUNT(*) > 1
+)
+AND (lat,lon) IN (
+    SELECT lat,lon
+    FROM Insurance
+    GROUP BY 1,2
+    HAVING COUNT(*) = 1
+)
