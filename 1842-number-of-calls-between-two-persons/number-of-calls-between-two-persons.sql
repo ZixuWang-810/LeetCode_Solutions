ada671120005e1b3611d9ce5170c03b4aeb8a3ff
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-28 20:24:49 -0400

    Time: 390 ms (72.59%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,13 +1,10 @@
 -- Write your PostgreSQL query statement below
 WITH cte AS (
-    SELECT
-        from_id,
-        to_id,
-        duration
+    SELECT *
     FROM Calls
     WHERE from_id < to_id
     UNION ALL
-    SELECT
+    SELECT 
         to_id,
         from_id,
         duration
@@ -15,9 +12,12 @@ WITH cte AS (
     WHERE from_id > to_id
 )
 SELECT
-    from_id AS person1,
+    from_id AS person1, 
     to_id AS person2,
-    COUNT(*) AS call_count ,
-    SUM(duration) AS total_duration 
+    COUNT(*) AS call_count,
+    SUM(duration) AS total_duration
 FROM cte
-GROUP BY 1, 2
+GROUP BY 
+    from_id,
+    to_id
+    
