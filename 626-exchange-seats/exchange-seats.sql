34176b7f90cc5a47e042daa664dd2af860eb4665
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-28 20:46:26 -0400

    Time: 179 ms (72.84%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,10 +1,10 @@
 -- Write your PostgreSQL query statement below
 SELECT
     CASE
-        WHEN id = (SELECT COUNT(*) FROM Seat) AND id % 2 = 1 THEN id
-        WHEN id % 2 = 1 THEN id + 1
-        ELSE id - 1 
+        WHEN id % 2 = 1 AND id = (SELECT COUNT(*) FROM Seat) THEN id
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id + 1
     END AS id,
     student
 FROM Seat
-ORDER BY id
+ORDER BY 1
