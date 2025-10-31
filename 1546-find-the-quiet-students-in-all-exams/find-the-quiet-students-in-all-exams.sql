4f4505b70374513e3a52f5922f0b3da57e42f1c7
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-31 11:14:59 -0400

    Time: 291 ms (9.65%) | Memory: 0B (100.00%) - LeetSync

--- 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
+++ 1546-find-the-quiet-students-in-all-exams/find-the-quiet-students-in-all-exams.sql
@@ -1,26 +1,27 @@
 -- Write your PostgreSQL query statement below
 WITH cte AS (
-    SELECT
+    SELECT 
         *,
-        RANK() OVER(
-            PARTITION BY exam_id ORDER BY score DESC
-        ) top_rank,
-        RANK() OVER(
-            PARTITION BY exam_id ORDER BY score
-        ) low_rank
+        RANK()OVER(
+            PARTITION BY exam_id
+            ORDER BY score
+        ) AS low,
+        RANK()OVER(
+            PARTITION BY exam_id
+            ORDER BY score DESC
+        ) AS high
     FROM Exam
 )
-SELECT 
+SELECT
     DISTINCT e.student_id,
     s.student_name
 FROM Exam e
 LEFT JOIN Student s
-    USING (student_id)
-WHERE e.student_id NOT IN (
-    SELECT
-        student_id
-    FROM cte 
-    WHERE top_rank = 1
-    OR low_rank = 1
+    USING(student_id)
+WHERE e.student_id NOT IN(
+    SELECT student_id
+    FROM cte
+    WHERE low = 1
+    OR high = 1
 )
 ORDER BY 1
