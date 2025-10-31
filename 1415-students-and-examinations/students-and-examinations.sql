5c8164824167fa59a7796a6680964c2099ca3b4c
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-31 16:56:09 -0400

    Time: 419 ms (38.68%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,18 +1,13 @@
 -- Write your PostgreSQL query statement below
-SELECT
+SELECT  
     s.student_id,
     s.student_name,
-    sub.subject_name,
-    SUM(
-        CASE 
-            WHEN e.subject_name IS NOT NULL THEN 1 
-            ELSE 0
-        END
-    ) AS attended_exams
+    su.subject_name,
+    COUNT(e.subject_name) AS attended_exams 
 FROM Students s
-CROSS JOIN Subjects sub
+CROSS JOIN Subjects su
 LEFT JOIN Examinations e
     ON s.student_id = e.student_id
-    AND sub.subject_name = e.subject_name
+    AND su.subject_name = e.subject_name
 GROUP BY 1,2,3
 ORDER BY 1,3
