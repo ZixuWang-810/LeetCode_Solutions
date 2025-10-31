46562370a831400eeaa4e690c53101efbbdf4b9c
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-10-31 13:14:41 -0400

    Time: 232 ms (12.44%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,6 +1,5 @@
 -- Write your PostgreSQL query statement below
-SELECT
-    *
+SELECT *
 FROM Patients
-WHERE conditions LIKE 'DIAB1%' 
-OR  conditions LIKE '% DIAB1%'
+WHERE conditions LIKE 'DIAB1%'
+OR conditions LIKE '% DIAB1%'
