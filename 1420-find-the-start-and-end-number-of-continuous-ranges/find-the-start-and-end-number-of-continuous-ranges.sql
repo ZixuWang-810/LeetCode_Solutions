e16b45a7e852e399fcdc28641b2091fad2ffb3d6
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-02 13:11:23 -0500

    Time: 247 ms (11.57%) | Memory: 0B (100.00%) - LeetSync

--- 1420-find-the-start-and-end-number-of-continuous-ranges/find-the-start-and-end-number-of-continuous-ranges.sql
+++ 1420-find-the-start-and-end-number-of-continuous-ranges/find-the-start-and-end-number-of-continuous-ranges.sql
@@ -1,13 +1,13 @@
 -- Write your PostgreSQL query statement below
-WITH grouped AS (
-    SELECT 
+WITH cte AS (
+    SELECT
         log_id,
-        log_id - ROW_NUMBER() OVER (ORDER BY log_id) AS grp
+        log_id - ROW_NUMBER()OVER() AS grp
     FROM Logs
 )
-SELECT 
+SELECT
     MIN(log_id) AS start_id,
     MAX(log_id) AS end_id
-FROM grouped
+FROM cte 
 GROUP BY grp
-ORDER BY start_id;
+ORDER BY 1
