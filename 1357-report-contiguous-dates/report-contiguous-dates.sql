6b9ee2637d88c6f056aace67f3a48bf9e7c65bd2
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-03 17:51:39 -0500

    Time: 531 ms (92.81%) | Memory: 0B (100.00%) - LeetSync

--- 1357-report-contiguous-dates/report-contiguous-dates.sql
+++ 1357-report-contiguous-dates/report-contiguous-dates.sql
@@ -1,32 +1,34 @@
 -- Write your PostgreSQL query statement below
-WITH events AS (
-SELECT 
-    fail_date AS event_date, 
-    'failed' AS event_state
-FROM Failed
-WHERE fail_date >= '2019-01-01' AND fail_date <= '2019-12-31'
-UNION ALL
-SELECT 
-    success_date , 
-    'succeeded'
-FROM Succeeded
-WHERE success_date >= '2019-01-01' AND success_date <= '2019-12-31'
-)
-, cte AS (
+WITH cte AS (
     SELECT
-        *,
-        ROW_NUMBER()OVER(
-            ORDER BY event_date
-        ) - ROW_NUMBER()OVER(
-            PARTITION BY event_state
-            ORDER BY event_state
-        ) AS grp
-    FROM events
+        fail_date AS period,
+        'failed' AS period_state
+    FROM Failed
+    WHERE fail_date >= '2019-01-01'
+    AND fail_date <= '2019-12-31'
+    UNION ALL
+    SELECT
+        success_date,
+        'succeeded'
+    FROM Succeeded
+    WHERE success_date >= '2019-01-01'
+    AND success_date <= '2019-12-31'
 )
-SELECT 
-    event_state AS period_state,
-    MIN(event_date) AS start_date,
-    MAX(event_date) AS end_date
-FROM cte
-GROUP BY event_state, grp
+,cte2 AS (
+    SELECT
+    *,
+     ROW_NUMBER()OVER(
+            ORDER BY period
+        ) - ROW_NUMBER()OVER(
+        PARTITION BY period_state
+        ORDER BY period
+    ) AS grp
+    FROM cte
+)
+SELECT
+    period_state,
+    MIN(period) AS start_date,
+    MAX(period) AS end_date
+FROM cte2
+GROUP BY 1, grp
 ORDER BY 2
