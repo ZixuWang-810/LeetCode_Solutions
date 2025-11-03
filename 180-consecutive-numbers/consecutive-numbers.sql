7b306829dd8b1330cb14d758e502e8bbc5d02e2e
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-03 18:13:56 -0500

    Time: 277 ms (42.84%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,13 +1,17 @@
 -- Write your PostgreSQL query statement below
 WITH cte AS (
-    SELECT 
-        *,
-        LEAD(num) OVER(ORDER BY id) AS next_num,
-        LAG(num) OVER(ORDER BY id) AS pre_num
+    SELECT
+        num,
+        LEAD(num) OVER(
+            ORDER BY id
+        ) AS nxt,
+        LAG(num)OVER(
+            ORDER BY id
+        ) AS prev
     FROM Logs
 )
 SELECT
-    DISTINCT num AS ConsecutiveNums
-FROM cte
-WHERE next_num = pre_num 
-AND next_num = num
+    DISTINCT num AS ConsecutiveNums 
+FROM cte 
+WHERE nxt = num
+AND num = prev
