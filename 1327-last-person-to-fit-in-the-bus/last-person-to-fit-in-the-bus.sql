9c78693d90b8e1521d2c18df1ce3d85fd5cdb43e
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-03 18:32:39 -0500

    Time: 428 ms (78.52%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,14 +1,16 @@
 -- Write your PostgreSQL query statement below
 WITH cte AS (
-    SELECT 
+    SELECT
         person_name,
         turn,
-        SUM(weight) OVER(ORDER BY turn) AS amount
+        SUM(weight)OVER(
+            ORDER BY turn
+        ) AS amount
     FROM Queue
 )
 SELECT
     person_name
-FROM cte 
+FROM cte
 WHERE amount <= 1000
 ORDER BY turn DESC
 LIMIT 1
