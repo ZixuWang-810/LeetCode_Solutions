41d33a047b26007057a79b597973600799ad6812
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-04 10:59:38 -0500

    Time: 176 ms (32.85%) | Memory: 0B (100.00%) - LeetSync

--- 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
+++ 1405-all-people-report-to-the-given-manager/all-people-report-to-the-given-manager.sql
@@ -1,13 +1,15 @@
 -- Write your PostgreSQL query statement below
-WITH recursive cte AS (
-    SELECT employee_id
-    FROM Employees 
-    WHERE manager_id = 1 AND employee_id != 1
+WITH RECURSIVE cte AS (
+    SELECT
+        employee_id
+    FROM Employees
+    WHERE manager_id = 1
+    AND employee_id != 1
     UNION
     SELECT
         e.employee_id
     FROM Employees e
-    JOIN cte c
-        ON e.manager_id = c.employee_id
+    JOIN cte c 
+        ON c.employee_id = e.manager_id
 )
 SELECT * FROM cte
