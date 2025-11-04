f44f1a4e5eb2885817ec67ffee9ac439365a0244
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-04 11:25:28 -0500

    Time: 179 ms (59.90%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,15 +1,12 @@
-WITH cte AS (
-    SELECT
-        sale_date,
+-- Write your PostgreSQL query statement below
+SELECT
+    sale_date,
+    SUM(
         CASE 
             WHEN fruit = 'oranges' THEN -sold_num
             ELSE sold_num
         END
-    FROM Sales
-)
-SELECT 
-    sale_date,
-    SUM(sold_num) AS diff
-FROM cte
-GROUP BY 1
-ORDER BY 1
+    ) AS diff
+FROM Sales
+GROUP BY sale_date
+ORDER BY sale_date
