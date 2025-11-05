1102de4c971f30d581464782c577a2275f843e3a
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-05 14:24:42 -0500

    Time: 304 ms (7.05%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -1,7 +1,10 @@
-SELECT product_name, SUM(unit) AS unit
-FROM Orders AS o
-INNER JOIN Products
-USING(product_id)
-WHERE TO_CHAR(order_date, 'YYYY-MM') = '2020-02'
-GROUP BY product_id, product_name
-HAVING SUM(unit) >= 100;
+-- Write your PostgreSQL query statement below
+SELECT
+    p.product_name,
+    SUM(o.unit) AS unit
+FROM Orders o
+LEFT JOIN Products p
+    USING(product_id)
+WHERE TO_CHAR(o.order_date, 'YYYY-MM') = '2020-02'
+GROUP BY 1 
+HAVING SUM(o.unit) >= 100
