6f41176ec540f2d128dca195b4d1017c15cc8654
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-06 14:05:41 -0500

    Time: 260 ms (18.28%) | Memory: 0B (100.00%) - LeetSync

--- 571-find-median-given-frequency-of-numbers/find-median-given-frequency-of-numbers.sql
+++ 571-find-median-given-frequency-of-numbers/find-median-given-frequency-of-numbers.sql
@@ -1,4 +1,22 @@
 -- Write your PostgreSQL query statement below
-select percentile_cont(0.5) within group (order by num) as median
-from (select num
-from numbers, generate_series(1, frequency))
+WITH cte AS (
+    SELECT
+        num,
+        SUM(frequency)OVER(ORDER BY num) - frequency AS lower_b,
+        SUM(frequency)OVER(ORDER BY num) AS upper_b
+    FROM Numbers
+)
+,cte2 AS (
+    SELECT
+        SUM(frequency)::DECIMAL/2 AS median
+    FROM Numbers
+)
+SELECT
+    ROUND(
+        AVG(num)
+        ,1
+    ) AS median
+FROM cte
+WHERE (
+    SELECT median FROM cte2
+) BETWEEN lower_b AND upper_b
