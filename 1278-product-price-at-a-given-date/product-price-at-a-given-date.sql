5d392e36d9a07f45d346fd469236c8ebe106f955
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-06 11:41:45 -0500

    Time: 346 ms (77.86%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,18 +1,20 @@
 -- Write your PostgreSQL query statement below
 WITH cte AS (
     SELECT
-        product_id,
-        new_price,
-        RANK() OVER(
-                PARTITION BY product_id ORDER BY change_date DESC
-            ) AS rank
+        *
     FROM Products
-    WHERE change_date <= '2019-08-16'
+    WHERE (product_id, change_date) IN (
+        SELECT
+            product_id,
+            MAX(change_date)
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY 1
+    )
 )
-
-SELECT 
+SELECT
     DISTINCT p.product_id,
     COALESCE(c.new_price, 10) AS price
 FROM Products p
-LEFT JOIN (SELECT * FROM cte WHERE rank = 1) c
-ON p.product_id = c.product_id
+LEFT JOIN cte c
+    USING(product_id)
