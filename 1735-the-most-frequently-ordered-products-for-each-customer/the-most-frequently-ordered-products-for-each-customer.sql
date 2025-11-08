1d07acccb5418d5a35b94485479df1707431aa4f
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-07 22:08:08 -0500

    Time: 597 ms (80.00%) | Memory: 0B (100.00%) - LeetSync

--- 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
+++ 1735-the-most-frequently-ordered-products-for-each-customer/the-most-frequently-ordered-products-for-each-customer.sql
@@ -1,13 +1,21 @@
-with cte as(
-    select  customer_id ,product_id, product_name,
-    rank()  over(partition by customer_id order by count(*)desc)rnk
-    from Customers 
-    join Orders 
-    using(customer_id)
-    join Products 
-    using(product_id)
-group by 1,2,3
+-- Write your PostgreSQL query statement below
+with cte as (
+    select
+        customer_id, 
+        product_id,
+        rank()over(
+            partition by customer_id
+            order by count(*) desc
+        )
+    from orders
+    group by 1,2
 )
-select customer_id ,product_id ,product_name 
-from cte
-where rnk=1
+select
+    c.customer_id,
+    c.product_id,
+    p.product_name
+from cte c
+left join products p
+    using (product_id)
+where rank = 1
+
