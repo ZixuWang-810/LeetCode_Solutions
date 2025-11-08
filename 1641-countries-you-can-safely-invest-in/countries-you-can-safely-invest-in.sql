2840c769f1ea8f2a35eb3f81b8ade57a4cf821c4
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-07 21:49:10 -0500

    Time: 491 ms (35.34%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,33 +1,31 @@
 -- Write your PostgreSQL query statement below
-WITH cte1 AS (
-    SELECT
-        caller_id,
-        callee_id,
-        duration
-    FROM Calls
-    UNION ALL
-    SELECT
-        callee_id,
-        caller_id,
-        duration
-    FROM Calls
+with cte as (
+    select
+        id,
+        name,
+        substring(phone_number from 1 for 3) as cc
+    from person
 )
-,cte2 AS (
-    SELECT
-        p.id,
-        c.country_code,
-        c.name
-    FROM Person p
-    LEFT JOIN Country c
-        ON SUBSTRING(p.phone_number FROM 1 FOR 3) = c.country_code
+, cte2 as (
+    select * from calls
+    union all
+    select
+        callee_id,
+        caller_id,
+        duration
+    from calls
 )
-SELECT
-    c2.name AS country
-FROM cte1 c1
-LEFT JOIN cte2 c2
-    ON c1.caller_id = c2.id
-GROUP BY 
-    c2.country_code,
-    1
-HAVING 
-    AVG(c1.duration) > (SELECT AVG(duration) FROM Calls)
+,cte3 as (
+    select
+        c.cc
+    from cte2 c2
+    left join cte c
+        on c2.caller_id = c.id
+    group by 1
+    having avg(duration) > (select avg(duration) from calls)
+)
+select name as country
+from country
+where country_code in (
+    select cc from cte3
+)
