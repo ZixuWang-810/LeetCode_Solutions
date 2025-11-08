c31e7eb166cd733445f05f856eb105994261b95b
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-08 10:32:11 -0500

    Time: 253 ms (22.34%) | Memory: 0B (100.00%) - LeetSync

--- 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -1,32 +1,31 @@
 -- Write your PostgreSQL query statement below
-WITH cte AS (
-    SELECT
+with cte as (
+    select
         host_team,
         guest_team,
         host_goals,
         guest_goals
-    FROM Matches
-    UNION ALL
-    SELECT
+    from matches
+    union all
+    select
         guest_team,
         host_team,
         guest_goals,
-        host_goals  
-    FROM Matches
+        host_goals
+    from matches
 )
-SELECT
+select
     t.team_id,
     t.team_name,
-    SUM(CASE 
-        WHEN c.host_goals > c.guest_goals THEN  3
-        WHEN c.host_goals = c.guest_goals THEN  1
-        ELSE 0
-    END) AS num_points
-FROM Teams t
-LEFT JOIN cte c
-    ON c.host_team = t.team_id
-GROUP BY 
-    1, 2
-ORDER BY 
-    num_points DESC,
-    1
+    sum(
+        case 
+            when host_goals > guest_goals then 3
+            when host_goals = guest_goals then 1
+            else 0
+        end
+    ) as num_points
+from teams t
+left join cte c
+    on t.team_id = c.host_team
+group by 1,2
+order by 3 desc, 1
