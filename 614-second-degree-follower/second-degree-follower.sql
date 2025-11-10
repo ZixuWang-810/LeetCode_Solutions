3078d5f97a42f3bd45fe51b58101e36e991969c6
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-09 20:00:29 -0500

    Time: 258 ms (46.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 614-second-degree-follower/second-degree-follower.sql
@@ -0,0 +1,12 @@
+-- Write your PostgreSQL query statement below
+select
+    followee as follower,
+    count(*) as num
+from follow
+where followee in (
+    select
+        follower
+    from follow
+)
+group by 1
+order by 1
