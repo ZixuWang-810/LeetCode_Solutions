691d837e0bd0d235e55b9c17230a7f049107f04f
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-11 11:36:59 -0500

    Time: 240 ms (32.35%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,15 +1,9 @@
 -- Write your PostgreSQL query statement below
-WITH cte AS (
-    SELECT
-        account,
-        SUM(amount) AS amount
-    FROM Transactions
-    GROUP BY account
-)
-SELECT 
-    u.name AS name,
-    c.amount AS balance
-FROM Users u
-LEFT JOIN cte c
-    ON c.account = u.account
-WHERE c.amount > 10000
+select
+    u.name,
+    sum(t.amount) as balance
+from transactions t
+left join users u
+    using(account)
+group by 1
+having sum(amount) > 10000
