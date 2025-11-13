ef8c044edf9a046c2a0aac984d99862ebf7cdf6b
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-13 17:42:03 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (69.36%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -5,7 +5,7 @@ class Solution:
 
         for i in range(len(words) - 1, -1, -1):
             res.append(words[i])
-            if i != 0:
-                res.append(" ")
+            # if i != 0:
+            #     res.append(" ")
 
-        return ''.join(res)
+        return ' '.join(res)
