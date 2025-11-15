84764ffeb156ed6dfcf2299e9460b855b9aa41db
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-15 12:37:20 -0500

    Time: 6 ms (47.20%) | Memory: 18.8 MB (49.75%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,10 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        a = b = 0
-        while b < len(nums):
-            if nums[b] != 0:
-                nums[a], nums[b] = nums[b], nums[a]
-                a += 1
-            b+=1
-        
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            # wait while we find a non-zero element to
+            # swap with you
+            if nums[slow] != 0:
+                slow += 1
