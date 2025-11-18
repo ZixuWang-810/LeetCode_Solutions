202d596bb235e0f0e14ee4ffb801459f74e2f700
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-17 19:28:59 -0500

    Time: 350 ms (80.91%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,7 +1,8 @@
 -- Write your PostgreSQL query statement below
-select distinct on (user_id)
+select 
     user_id,
     MAX(time_stamp) last_stamp
 from logins
 where to_char(time_stamp, 'YYYY') = '2020'
 group by 1
+order by 1
