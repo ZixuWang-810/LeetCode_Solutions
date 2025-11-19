d306db57e51132bcd45e990a7c7296a16c546def
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-19 12:41:13 -0500

    Time: 210 ms (42.25%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,5 @@
 -- Write your PostgreSQL query statement below
-SELECT(
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    LIMIT 1 OFFSET 1
-)
-AS SecondHighestSalary
+select (select distinct salary 
+from employee
+order by salary desc
+limit 1 offset 1) SecondHighestSalary
