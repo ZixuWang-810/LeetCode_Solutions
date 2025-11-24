e39d6d689534d4f815e3d3d66ac27806c41dc16c
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-23 19:09:28 -0500

    Time: 6 ms (65.50%) | Memory: 18.9 MB (35.63%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -3,13 +3,8 @@ class Solution:
         res = []
         for i in asteroids:
             while res and res[-1] > 0 and i < 0:
-                if res[-1] + i < 0:
-                    res.pop()
-                elif res[-1]+i == 0:
-                    res.pop()
-                    break
-                else:
-                    break
-            else:
-                res.append(i)
+                if res[-1] + i < 0: res.pop()
+                elif res[-1]+i == 0: res.pop(); break
+                else: break
+            else: res.append(i)
         return res
