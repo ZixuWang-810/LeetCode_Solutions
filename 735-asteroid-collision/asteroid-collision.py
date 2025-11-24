0211044c5bdd7a13c45e2aac3127f7726e62e622
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-23 19:08:31 -0500

    Time: 3 ms (93.72%) | Memory: 18.7 MB (84.21%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,10 +1,15 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        s = []
-        for a in asteroids:
-            while s and s[-1] > 0 and a < 0:
-                if s[-1] + a < 0: s.pop()
-                elif s[-1] + a > 0: break    
-                else: s.pop(); break
-            else: s.append(a)        
-        return s
+        res = []
+        for i in asteroids:
+            while res and res[-1] > 0 and i < 0:
+                if res[-1] + i < 0:
+                    res.pop()
+                elif res[-1]+i == 0:
+                    res.pop()
+                    break
+                else:
+                    break
+            else:
+                res.append(i)
+        return res
