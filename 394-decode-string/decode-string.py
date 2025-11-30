d2babf29c3e4db77e903c501aca69d451138deba
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-11-30 12:03:39 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (84.23%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,16 +1,27 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        res = []
-        for i in s:
-            temp = ''
-            if i == ']':
-                while res[-1] != '[':
-                    temp = res.pop()+temp
-                res.pop()
-                count = ''
-                while res and res[-1].isdigit():
-                    count = res.pop() + count
-                res.append(int(count)*temp)
+        num_stack = []
+        str_stack = []
+        current_num = 0
+        current_str = ''
+        
+        for char in s:
+            if char.isdigit():
+                current_num = current_num * 10 + int(char)
+            elif char == '[':
+                # Push current state to stacks
+                num_stack.append(current_num)
+                str_stack.append(current_str)
+                # Reset for new level
+                current_num = 0
+                current_str = ''
+            elif char == ']':
+                # Pop and decode
+                num = num_stack.pop()
+                prev_str = str_stack.pop()
+                current_str = prev_str + current_str * num
             else:
-                res.append(i)
-        return ''.join(res)
+                # Regular character
+                current_str += char
+        
+        return current_str
