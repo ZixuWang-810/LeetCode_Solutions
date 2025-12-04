1e423c67a1d6de034aa0e9af4537e25ee7278aee
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-03 23:30:48 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (6.73%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,16 +1,17 @@
 class Solution:
-    def compress(self, chars: List[str]) -> int:
-        left = right = 0
-        while right < len(chars):
-            count = 0
-            s = chars[right]
-            while right < len(chars) and chars[right] == s:
-                count+=1
-                right+=1
-            chars[left] = s
-            left += 1
-            if count > 1:
-                for i in str(count):
-                    chars[left] = i
-                    left+=1
-        return left
+  def compress(self, chars: List[str]) -> int:
+    ans = 0
+    i = 0
+    while i < len(chars):
+      letter = chars[i]
+      count = 0
+      while i < len(chars) and chars[i] == letter:
+        count += 1
+        i += 1
+      chars[ans] = letter
+      ans += 1
+      if count > 1:
+        for c in str(count):
+          chars[ans] = c
+          ans += 1
+    return ans
