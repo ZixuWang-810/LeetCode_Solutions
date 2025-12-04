f6473d81c03af0df238c5d7d929611e308e2f2bc
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-03 19:00:40 -0500

    Time: 190 ms (98.82%) | Memory: 0B (100.00%) - LeetSync

--- 579-find-cumulative-salary-of-an-employee/find-cumulative-salary-of-an-employee.sql
+++ 579-find-cumulative-salary-of-an-employee/find-cumulative-salary-of-an-employee.sql
@@ -1,16 +1,23 @@
 -- Write your PostgreSQL query statement below
+with cte as (
+    select
+        id,
+        month,
+        salary,
+        rank()over(
+            partition by id
+            order by month desc
+        ) rank
+    from employee
+)
 select
     id,
     month,
     sum(salary)over(
         partition by id
-        order by month 
+        order by month
         range between 2 preceding and current row
-    ) as Salary
-from employee
-where (id, month) not in (
-    select id, max(month)
-    from employee
-    group by 1
-)
+    ) salary
+from cte 
+where rank != 1
 order by id, month desc
