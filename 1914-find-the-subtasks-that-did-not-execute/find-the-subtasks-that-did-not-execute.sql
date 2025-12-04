8cb31b6acc06637dab811e1c0bac629e7d42132a
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-03 19:06:57 -0500

    Time: 292 ms (56.62%) | Memory: 0B (100.00%) - LeetSync

--- 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
+++ 1914-find-the-subtasks-that-did-not-execute/find-the-subtasks-that-did-not-execute.sql
@@ -2,9 +2,9 @@
 -- Write your PostgreSQL query statement below
 select 
     task_id,
-    GENERATE_SERIES(1, subtasks_count, 1) AS subtask_id
+    generate_series(1, subtasks_count, 1) AS subtask_id
 from Tasks
 except (
     select task_id, subtask_id
-    from  Executed
+    from  executed
 )
