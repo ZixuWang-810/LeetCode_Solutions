bd648bca1102bca7c9cf24b1ea2fb8d6f702810b
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-04 19:19:41 -0500

    Time: 511 ms (7.83%) | Memory: 18.7 MB (15.44%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -6,15 +6,14 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        def helper(root, sum):
-            if not root:
+        def helper(rt, tar):
+            if not rt:
                 return 0
-            count = 1 if root.val == sum else 0
-            
-            count += helper(root.left, sum-root.val)
-            count += helper(root.right, sum-root.val)
+            if rt.val == tar: count = 1
+            else: count = 0
+            count += helper(rt.left, tar - rt.val)
+            count += helper(rt.right, tar - rt.val)
             return count
-        
-        if not root:
-            return 0
-        return helper(root, targetSum)+ self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum)
+
+        if not root:return 0
+        return helper(root, targetSum) + self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum)
