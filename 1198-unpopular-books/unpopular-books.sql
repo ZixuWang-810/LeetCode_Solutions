c33da87421d8c8e4bccbfb58980a8331a6fdc02c
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-04 23:52:01 -0500

    Time: 534 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 1198-unpopular-books/unpopular-books.sql
+++ 1198-unpopular-books/unpopular-books.sql
@@ -1,14 +1,27 @@
- -- Write your PostgreSQL query statement below
-with cte as
-(Select book_id,sum(quantity) as total_sold
-from Orders
-WHERE dispatch_date >= DATE '2018-06-23' 
-  AND dispatch_date < DATE '2019-06-23'
-group by book_id)
-
-select b.book_id,b.name
-from Books b
-left join cte as c
-on b.book_id = c.book_id
-where COALESCE(total_sold,0) <10 
-and available_from < ('2019-05-23')
+-- Write your PostgreSQL query statement below
+with cte1 as (
+    select
+        book_id,
+        name
+    from books 
+    where available_from < '2019-05-23'
+),
+cte2 as (
+    select
+        book_id,
+        sum(quantity) as quantity
+    from orders
+    where dispatch_date between '2018-06-23' 
+    and  '2019-06-23'
+    group by 1
+)
+select 
+    b.book_id,
+    b.name
+from books b
+left join cte2 c
+    using(book_id)
+where coalesce(c.quantity, 0) < 10
+and book_id in (
+    select book_id from cte1
+)
