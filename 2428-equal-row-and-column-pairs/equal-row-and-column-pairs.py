d591f7ea9457c39291d0c19bf9c8660b30a2ff57
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-07 15:31:04 -0500

    Time: 20 ms (74.82%) | Memory: 22 MB (60.09%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.py
@@ -1,12 +1,15 @@
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        r_dic = {}
-        res = 0
-        for r in grid:
-            r_dic[str(r)] = r_dic.get(str(r), 0) + 1
+        row, column = defaultdict(int), defaultdict(int)
+        for i in grid:
+            row[tuple(i)] += 1
         for i in range(len(grid)):
-            col = []
+            temp = []
             for j in range(len(grid)):
-                col.append(grid[j][i])
-            res += r_dic.get(str(col), 0)
+                temp.append(grid[j][i])
+            column[tuple(temp)] +=1
+        res = 0
+        for r in row:
+            if r in column:
+                res+= row[r] * column[r]
         return res
