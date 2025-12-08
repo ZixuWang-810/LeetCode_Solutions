ee7c7b3acd9840dd13445f5f28934d2f5696caaa
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-08 11:11:10 -0500

    Time: 2842 ms (5.88%) | Memory: 0B (100.00%) - LeetSync

--- 1225-active-businesses/active-businesses.sql
+++ 1225-active-businesses/active-businesses.sql
@@ -1,20 +1,20 @@
 -- Write your PostgreSQL query statement below
-with cte as (
+with cte1 as (
     select 
         event_type,
-        avg(occurrences)::decimal
+        round(avg(occurrences)::decimal,1) average
     from events
-    group by event_type
-)
-,cte2 as (
-    select 
-        e.business_id,
-        sum(case when e.occurrences > c.avg then 1 else 0 end ) as cnt
-    from events e 
-    left join cte c
-    on c.event_type = e.event_type 
     group by 1
 )
+, cte2 as (
+    select
+        business_id,
+        case when e.occurrences > c.average then 1 else 0 end as count
+    from events e
+    left join cte1 c
+        using (event_type)
+)
 select business_id
-from cte2
-where cnt > 1
+from cte2 
+group by 1
+having sum(count) > 1
