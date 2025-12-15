1a60b3681dec3a7beecca96d170c33cc8348e965
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-15 15:23:06 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (36.84%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,24 +1,16 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        results = []
-
-        def backtrack(remain, comb, next_start):
-            if remain == 0 and len(comb) == k:
-                # make a copy of current combination
-                # Otherwise the combination would be reverted in other branch of backtracking.
-                results.append(list(comb))
+        def backtrack(index, num, tar):
+            if tar == 0 and len(num) == k: 
+                res.append(num.copy())
                 return
-            elif remain < 0 or len(comb) == k:
-                # exceed the scope, no need to explore further.
-                return
+            elif len(num) == k or tar < 0: return
 
-            # Iterate through the reduced list of candidates.
-            for i in range(next_start, 9):
-                comb.append(i + 1)
-                backtrack(remain - i - 1, comb, i + 1)
-                # backtrack the current choice
-                comb.pop()
-
-        backtrack(n, [], 0)
-
-        return results
+            for i in range(index, 9):
+                num.append(i+1)
+                backtrack(i+1, num, tar-i-1)
+                num.pop()
+        res = []
+        backtrack(0, [], n)
+        return res
+            
