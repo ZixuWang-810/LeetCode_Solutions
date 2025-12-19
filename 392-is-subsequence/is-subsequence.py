7b35434a8b9764fab92f995c5aec0c4ba45039be
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-18 22:01:36 -0500

    Time: 3 ms (26.28%) | Memory: 17.2 MB (100.00%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -3,9 +3,8 @@ class Solution:
         slow, fast = 0, 0
         while slow < len(s):
             if fast == len(t): return False
-            else:
-                if s[slow] == t[fast]:
-                    slow+=1
-                    fast+=1
-                else: fast+=1
+            if s[slow] == t[fast]:
+                slow+=1
+                fast+=1
+            else: fast += 1
         return True
