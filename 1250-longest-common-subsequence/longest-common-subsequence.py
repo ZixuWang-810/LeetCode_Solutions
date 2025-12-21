fbc51c7bf001c980400b89f3bb080d64c72a1480
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-21 10:23:29 -0500

    Time: 1187 ms (15.62%) | Memory: 310.7 MB (6.87%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,5 +1,4 @@
 from functools import lru_cache
-
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
@@ -11,18 +10,12 @@ class Solution:
             if p1 == len(text1) or p2 == len(text2):
                 return 0
             
-            # Option 1: We don't include text1[p1] in the solution.
-            option_1 = memo_solve(p1 + 1, p2)
+            # Recursive case 1.
+            if text1[p1] == text2[p2]:
+                return 1 + memo_solve(p1 + 1, p2 + 1)
             
-            # Option 2: We include text1[p1] in the solution, as long as
-            # a match for it in text2 at or after p2 exists.
-            first_occurence = text2.find(text1[p1], p2)
-            option_2 = 0
-            if first_occurence != -1:
-                option_2 = 1 + memo_solve(p1 + 1, first_occurence + 1)
+            # Recursive case 2.
+            else:
+                return max(memo_solve(p1, p2 + 1), memo_solve(p1 + 1, p2))
             
-            # Return the best option.
-            return max(option_1, option_2)
-                
         return memo_solve(0, 0)
-        
