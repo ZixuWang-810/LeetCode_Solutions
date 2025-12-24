774f32c44f534f4aff0204dcdde1af61a2f516bc
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-24 16:48:37 -0500

    Time: 627 ms (9.46%) | Memory: 19.4 MB (87.22%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,10 +1,9 @@
-
-class Solution:
+class Solution(object):
     def singleNumber(self, nums: List[int]) -> int:
-        hash_table = defaultdict(int)
+        no_duplicate_list = []
         for i in nums:
-            hash_table[i] += 1
-
-        for i in hash_table:
-            if hash_table[i] == 1:
-                return i
+            if i not in no_duplicate_list:
+                no_duplicate_list.append(i)
+            else:
+                no_duplicate_list.remove(i)
+        return no_duplicate_list.pop()
