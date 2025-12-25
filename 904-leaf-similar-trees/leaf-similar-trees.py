34a14d6123e03e212a71c44e4770bee3f79f371a
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2025-12-25 17:45:03 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (94.31%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -8,12 +8,10 @@ class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
         leaf1, leaf2 = [], []
         def helper(root, leaf):
-            if not root:
-                return
+            if not root: return
             if not root.left and not root.right: leaf.append(root.val)
             helper(root.left, leaf)
             helper(root.right, leaf)
-            return
         helper(root1, leaf1)
         helper(root2, leaf2)
         return leaf1 == leaf2
