1e46cc57bd28382863cf81913c299e9cb6dd7270
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-04 15:33:38 -0600

    Time: 0 ms (100.00%) | Memory: 17.4 MB (85.34%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,21 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         res = []
-        dic = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz',
+        letters = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
         }
-        def backtrack(index, path):
-            if len(path) == len(digits):
-                res.append("".join(path))
+        def helper(remain, comb):
+            if len(remain) == 0:
+                res.append(comb)
                 return
-            for letter in dic[digits[index]]:
-                backtrack(index+1, path+letter)
-        backtrack(0, '')
+            for i in letters[remain[0]]:
+                helper(remain[1:], comb+i)
+        helper(digits, '')
         return res
