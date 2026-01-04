0fdde0db2bc035fa3ffd951e1c283418555233ad
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-04 11:04:14 -0600

    Time: 377 ms (11.76%) | Memory: 0B (100.00%) - LeetSync

--- 569-median-employee-salary/median-employee-salary.sql
+++ 569-median-employee-salary/median-employee-salary.sql
@@ -7,32 +7,31 @@ with cte1 as (
         row_number()over(
             partition by company
             order by salary
-        ) rn
+        ) as rn
     from employee
-),
-cte2 as (
+)
+,cte2 as (
     select
         company,
-        max(rn) / 2 + 1 as m1,
-        case when max(rn) % 2 = 0 then max(rn) / 2 
-        else 0 end as m2
+        max(rn) / 2 + 1 as med1,
+        case when max(rn) % 2 = 0 then max(rn) / 2 else 0 end as med2
     from cte1
     group by 1
 )
 select
-    id, 
+    id,
     company,
     salary
 from cte1
 where (company, rn) in (
     select
         company,
-        m1
+        med1
     from cte2
 )
 or (company, rn) in (
     select
         company,
-        m2
+        med2
     from cte2
 )
