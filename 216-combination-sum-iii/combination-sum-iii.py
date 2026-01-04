afb1912eab4257e571acb047bdb82f8a653e74d9
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-04 10:29:49 -0600

    Time: 0 ms (100.00%) | Memory: 17.1 MB (99.73%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,16 +1,15 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        def backtrack(index, num, tar):
-            if tar == 0 and len(num) == k: 
-                res.append(num.copy())
-                return
-            elif len(num) == k or tar < 0: return
-
-            for i in range(index, 9):
-                num.append(i+1)
-                backtrack(i+1, num, tar-i-1)
-                num.pop()
         res = []
-        backtrack(0, [], n)
+        def backtrack(comb, remain, start):
+            if remain == 0 and len(comb) == k:
+                res.append(list(comb))
+                return
+            if remain < 0: return
+            if len(comb) > k: return
+            for i in range(start+1, 10):
+                comb.append(i)
+                backtrack(comb, remain - i, i)
+                comb.pop()
+        backtrack([], n, 0)
         return res
-            
