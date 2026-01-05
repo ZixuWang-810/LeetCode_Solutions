4358b79939f0f3f0004d069f5a07710d8aeabc16
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-05 09:56:16 -0600

    Time: 7 ms (97.91%) | Memory: 20.5 MB (77.55%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,6 +2,5 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         dic = defaultdict(list)
         for i in strs:
-            temp = sorted(i)
-            dic[tuple(temp)].append(i)
+            dic[tuple(sorted(i))].append(i)
         return list(dic.values())
