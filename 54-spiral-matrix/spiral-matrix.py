58d9f7d3026318acafdcf0e0cee0a193bddd2325
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-06 17:05:09 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (12.16%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,13 +1,35 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        x, y, dx, dy = 0, 0, 1, 0
-        m, n = len(matrix), len(matrix[0])
-        res = []
-        for i in range(m*n):
-            res.append(matrix[y][x])
-            matrix[y][x] = "."
-            if not 0 <= x+dx < n or not 0 <= y+dy < m or matrix[y+dy][x+dx] == '.':
-                dx, dy = -dy, dx
-            x+=dx
-            y+=dy
-        return res
+        result = []
+        rows, columns = len(matrix), len(matrix[0])
+        up = left = 0
+        right = columns - 1
+        down = rows - 1
+
+        while len(result) < rows * columns:
+            # Traverse from left to right.
+            for col in range(left, right + 1):
+                result.append(matrix[up][col])
+
+            # Traverse downwards.
+            for row in range(up + 1, down + 1):
+                result.append(matrix[row][right])
+
+            # Make sure we are now on a different row.
+            if up != down:
+                # Traverse from right to left.
+                for col in range(right - 1, left - 1, -1):
+                    result.append(matrix[down][col])
+
+            # Make sure we are now on a different column.
+            if left != right:
+                # Traverse upwards.
+                for row in range(down - 1, up, -1):
+                    result.append(matrix[row][left])
+
+            left += 1
+            right -= 1
+            up += 1
+            down -= 1
+
+        return result
