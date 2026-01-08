8c4e16935322a6b818dde3ac292169259b5b9730
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-07 22:29:56 -0500

    Time: 9 ms (40.66%) | Memory: 22.5 MB (12.23%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -3,10 +3,9 @@ class Solution:
         intervals.sort(key =  lambda x: x[0])
         res = []
         for i in intervals:
-            if not res: res.append(i)
-            if res[-1][1] >= i[0]:
+            if not res or res[-1][1] < i[0]:
+                res.append(i)
+            else:
                 if res[-1][1] < i[1]:
                     res[-1][1] = i[1]
-            else:
-                res.append(i)
         return res
