7664df01ee6bfafe3f0d1acb02e4385a3ca02976
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-21 21:14:16 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.19%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,16 +1,16 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         res = []
-        for char in s:
-            if char != ']': res.append(char)
-            else:
+        for i in s:
+            if i == ']':
                 temp = ''
-                while res[-1] != '[':
+                while res and res[-1] != '[':
                     temp = res.pop() + temp
-                nums = ''
                 res.pop()
+                nums = ''
                 while res and res[-1].isdigit():
                     nums = res.pop()+nums
-                temp = int(nums) * temp
+                temp = int(nums)*temp
                 res.append(temp)
+            else: res.append(i)
         return ''.join(res)
