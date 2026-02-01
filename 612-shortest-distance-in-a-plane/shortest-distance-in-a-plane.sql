9867fc588ea5a4c31de11d60db590b1528f9867e
ZixuWang-810 <85013326+ZixuWang-810@users.noreply.github.com>
2026-01-31 21:12:11 -0500

    Time: 231 ms (97.44%) | Memory: 0B (100.00%) - LeetSync

--- 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
+++ 612-shortest-distance-in-a-plane/shortest-distance-in-a-plane.sql
@@ -1,12 +1,10 @@
 -- Write your PostgreSQL query statement below
 select
     round(
-        sqrt(power((p1.x - p2.x), 2) + power((p1.y - p2.y), 2))::decimal
+        min(sqrt(power((p1.x - p2.x), 2) + power((p1.y - p2.y), 2)))::decimal
         ,2
     ) as shortest
 from point2d p1
 cross join point2d p2
 where p1.x != p2.x
 or p1.y != p2.y
-order by 1
-limit 1
